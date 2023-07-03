175c6832d406713853da2c1ca1ec9c5d8ab99b47
Joshua Vaneps <joshuavaneps@yahoo.com>
2023-07-03 18:46:41 -0400

    was able to fix the testing

--- utils/shape.js
+++ utils/shape.js
@@ -1,4 +1,4 @@
-// idea 2 is to try using a class with multiple function and maybe calling those multiple functions
+// using a class with multiple function and calling those multiple functionsin generate logos
 // in a switch case on my generate logos javasctipt
 class Shape {
   constructor(shapeFill, textColor, text) {
@@ -6,43 +6,70 @@ class Shape {
     this.textColor = textColor;
     this.text = text;
   }
-
-  renderTriangle() {
-    console.log("you picked triangle");
-    return `<svg version="1.1" width="600" height="400" xmlns="http://www.w3.org/2000/svg">
-      <polygon points="150,20 250,200 50,200" fill="${shapeFill}"/>
-       <text x="150" y="145" font-size="40" text-anchor="middle" fill="${textColor}">${text}</text>
-    </svg>`;
-  }
-
-  renderSquare() {
-    console.log("you picked square");
-    return `<svg version="1.1" width="600" height="400" xmlns="http://www.w3.org/2000/svg">
-            <rect x="50" y="20" rx="20" ry="20" width="200" height="200" fill="${shapeFill}" />
-             <text x="150" y="140" font-size="60" text-anchor="middle" fill="${textColor}">${text}</text>
-          </svg>`;
-  }
 }
 
-// idea 3 having another class inherit my stuff from shape and then render inside of circle
-class Circle {
-  constructor(shapeFill, textColor, text) {
-    Shape.call(this, shapeFill, textColor, text);
-  }
-  renderCircle() {
-    return `
-          <svg version="1.1" width="600" height="400" xmlns="http://www.w3.org/2000/svg">
-          <circle cx="150" cy="120" r="100" fill="${shapeFill}" />
-          <text x="150" y="125" font-size="60" text-anchor="middle" fill="${textColor}">${text}</text>
-          </svg>`;
-  }
-}
-
-// Idea 1 i had was to make a prototype
-// Shape.prototype.renderCircle = function () {
+// renderCircle() {
+//   console.log("you picked circle");
 //   return `
 //     <svg version="1.1" width="600" height="400" xmlns="http://www.w3.org/2000/svg">
-//     <circle cx="150" cy="120" r="100" fill="${shapeFill}" />
-//     <text x="150" y="125" font-size="60" text-anchor="middle" fill="${textColor}">${text}</text>
+//       <circle cx="150" cy="120" r="100" fill="${this.shapeFill}" />
+//       <text x="150" y="125" font-size="60" text-anchor="middle" fill="${this.textColor}">${this.text}</text>
 //     </svg>`;
-// };
+// }
+
+// renderTriangle() {
+//   console.log("you picked triangle");
+//   return `<svg version="1.1" width="600" height="400" xmlns="http://www.w3.org/2000/svg">
+//     <polygon points="150,20 250,200 50,200" fill="${this.shapeFill}"/>
+//     <text x="150" y="145" font-size="40" text-anchor="middle" fill="${this.textColor}">${this.text}</text>
+//   </svg>`;
+// }
+
+// renderSquare() {
+//   console.log("you picked square");
+//   return `<svg version="1.1" width="600" height="400" xmlns="http://www.w3.org/2000/svg">
+//     <rect x="50" y="20" rx="20" ry="20" width="200" height="200" fill="${this.shapeFill}" />
+//     <text x="150" y="140" font-size="60" text-anchor="middle" fill="${this.textColor}">${this.text}</text>
+//   </svg>`;
+// }
+
+class Circle extends Shape {
+  constructor(shapeFill, textColor, text) {
+    super(shapeFill, textColor, text);
+  }
+  renderCircle() {
+    console.log("you picked circle");
+    return `<svg version="1.1" width="600" height="400" xmlns="http://www.w3.org/2000/svg">
+    <circle cx="150" cy="120" r="100" fill="${this.shapeFill}" />
+    <text x="150" y="125" font-size="60" text-anchor="middle" fill="${this.textColor}">${this.text}</text>
+  </svg>`;
+  }
+}
+
+class Triangle extends Shape {
+  constructor(shapeFill, textColor, text) {
+    super(shapeFill, textColor, text);
+  }
+  renderTriangle() {
+    console.log("you picked Triangle");
+    return `<svg version="1.1" width="600" height="400" xmlns="http://www.w3.org/2000/svg">
+      <polygon points="150,20 250,200 50,200" fill="${this.shapeFill}"/>
+      <text x="150" y="145" font-size="40" text-anchor="middle" fill="${this.textColor}">${this.text}</text>
+    </svg>`;
+  }
+}
+
+class Square extends Shape {
+  constructor(shapeFill, textColor, text) {
+    super(shapeFill, textColor, text);
+  }
+  renderSquare() {
+    console.log("you picked Square");
+    return `<svg version="1.1" width="600" height="400" xmlns="http://www.w3.org/2000/svg">
+    <rect x="50" y="20" rx="20" ry="20" width="200" height="200" fill="${this.shapeFill}" />
+    <text x="150" y="140" font-size="60" text-anchor="middle" fill="${this.textColor}">${this.text}</text>
+  </svg>`;
+  }
+}
+
+module.exports = { Shape, Circle, Triangle, Square };
--- utils/shapes.test.js
+++ utils/shapes.test.js
@@ -1,115 +1,44 @@
-const generate = require("./generateLogo");
-const data = require("../index").data;
+const Shape = require("./shape");
+const { Circle, Square, Triangle } = require("./shape");
 
-describe("generate", () => {
-  test("check rendering of logo", () => {
-    var shapeFill = blue;
-    const expectedGenerate = (
-      <svg
-        version="1.1"
-        width="600"
-        height="400"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <polygon points="150,20 250,200 50,200" fill="${shapeFill}" />
-        <text
-          x="150"
-          y="145"
-          font-size="40"
-          text-anchor="middle"
-          fill="${data.textColor}"
-        >
-          ${data.text}
-        </text>
-      </svg>
-    );
-    expect(generate.render()).toEqual(expectedGenerate);
+describe("Triangle", () => {
+  test("checks rendering of triangle", () => {
+    const shapeFill = "blue";
+    const textColor = "white";
+    const text = "asw";
+    const triangle = new Triangle(shapeFill, textColor, text);
+    const expectedShape = `<svg version="1.1" width="600" height="400" xmlns="http://www.w3.org/2000/svg">
+      <polygon points="150,20 250,200 50,200" fill="${shapeFill}"/>
+      <text x="150" y="145" font-size="40" text-anchor="middle" fill="${textColor}">${text}</text>
+    </svg>`;
+    expect(triangle.renderTriangle()).toEqual(expectedShape);
   });
 });
 
-// describe("Triangle", () => {
-//   test("checks rendering of triangle", () => {
-//     var shapeFill = "blue";
-//     const triangle = new Triangle(shapeFill);
-//     const expectedShape = (
-//       <svg
-//         version="1.1"
-//         width="600"
-//         height="400"
-//         xmlns="http://www.w3.org/2000/svg"
-//       >
-//         <polygon points="150,20 250,200 50,200" fill="${shapeFill}" />
-//         <text
-//           x="150"
-//           y="145"
-//           font-size="40"
-//           text-anchor="middle"
-//           fill="${data.textColor}"
-//         >
-//           ${data.text}
-//         </text>
-//       </svg>
-//     );
-//     expect(triangle.render()).toEqual(expectedShape);
-//   });
-// });
-// describe("Square", () => {
-//   test("checks rendering of square", () => {
-//     const shapeFill = "red";
-//     const square = new Square(shapeFill);
-//     const expectedShape = (
-//       <svg
-//         version="1.1"
-//         width="600"
-//         height="400"
-//         xmlns="http://www.w3.org/2000/svg"
-//       >
-//         <rect
-//           x="50"
-//           y="20"
-//           rx="20"
-//           ry="20"
-//           width="200"
-//           height="200"
-//           fill="${shapeFill}"
-//         />
-//         <text
-//           x="150"
-//           y="140"
-//           font-size="60"
-//           text-anchor="middle"
-//           fill="${data.textColor}"
-//         >
-//           ${data.text}
-//         </text>
-//       </svg>
-//     );
-//     expect(square.render()).toEqual(expectedShape);
-//   });
-// });
-// describe("Circle", () => {
-//   test("checks rendering of circle", () => {
-//     const shapeFill = "yellow";
-//     const circle = new Circle(shapeFill);
-//     const expectedShape = (
-//       <svg
-//         version="1.1"
-//         width="600"
-//         height="400"
-//         xmlns="http://www.w3.org/2000/svg"
-//       >
-//         <polygon points="150,20 250,200 50,200" fill="${shapeFill}" />
-//         <text
-//           x="150"
-//           y="145"
-//           font-size="40"
-//           text-anchor="middle"
-//           fill="${data.textColor}"
-//         >
-//           ${data.text}
-//         </text>
-//       </svg>
-//     );
-//     expect(circle.render()).toEqual(expectedShape);
-//   });
-// });
+describe("Square", () => {
+  test("checks rendering of Square", () => {
+    const shapeFill = "blue";
+    const textColor = "white";
+    const text = "asw";
+    const square = new Square(shapeFill, textColor, text);
+    const expectedShape = `<svg version="1.1" width="600" height="400" xmlns="http://www.w3.org/2000/svg">
+    <rect x="50" y="20" rx="20" ry="20" width="200" height="200" fill="${shapeFill}" />
+    <text x="150" y="140" font-size="60" text-anchor="middle" fill="${textColor}">${text}</text>
+  </svg>`;
+    expect(square.renderSquare()).toEqual(expectedShape);
+  });
+});
+
+describe("Circle", () => {
+  test("checks rendering of Circle", () => {
+    const shapeFill = "blue";
+    const textColor = "white";
+    const text = "asw";
+    const circle = new Circle(shapeFill, textColor, text);
+    const expectedShape = `<svg version="1.1" width="600" height="400" xmlns="http://www.w3.org/2000/svg">
+    <circle cx="150" cy="120" r="100" fill="${shapeFill}" />
+    <text x="150" y="125" font-size="60" text-anchor="middle" fill="${textColor}">${text}</text>
+  </svg>`;
+    expect(circle.renderCircle()).toEqual(expectedShape);
+  });
+});
